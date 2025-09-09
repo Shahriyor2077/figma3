65462e4aa0149b2c0b67ddd8992ccb874cd3a677
Shahriyor <shahriyor@example.com>
2025-09-09 13:09:59 +0500

    .

--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -1,9 +1,10 @@
+import hero from "../../components/hero.jpg"
 
 const Hero = () => {
   return (
     <section
       className="relative h-[640px] bg-cover bg-center flex items-center justify-center"
-      style={{backgroundImage: "url('/src/assets/hero.jpg"}}
+      style={{ backgroundImage: `url(${hero})` }}
     >
       <div className="bg-white rounded-2xl shadow-lg p-10 text-center max-w-xl">
         <p className="text-lime-600 mb-2">Say Hello to ToyStore!</p>
